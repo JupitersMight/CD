9bd2f68440370339485eeafa1b08aa7d85db1028
JupitersMight <leonardo_duarte_rodrigues@hotmail.com>
2018-11-05 16:28:49 +0000

    fixed SMOTE

--- smote.py
+++ smote.py
@@ -4,7 +4,7 @@ from imblearn.over_sampling import SMOTE
 #          WARNING !!! In order to use SMOTE the dataset must not contain missing values          #
 
 # FILES
-NAME_OF_DATASET_FILE = 'C:\\Users\\Leona\\PycharmProjects\\LABS\\project\\apstraining.csv'
+NAME_OF_DATASET_FILE = 'C:\\Users\\Leona\\PycharmProjects\\LABS\\project\\aps_training_average.csv'
 NAME_OF_SMOTE_OVER_SAMPLING_FILE = 'smote_over_sampling_aps_training.csv'
 
 # CLASS ATTRIBUTE
@@ -19,7 +19,9 @@ X = df_train.iloc[:, df_train.columns != NAME_OF_CLASS_ATTRIBUTE].values
 Y = df_train.iloc[:, INDEX_OF_CLASS_ATTRIBUTE]
 smote = SMOTE(ratio='minority')
 X_res, Y_res = smote.fit_sample(X, Y)
-result = Y_res.append(X_res)
+result = pd.concat([pd.Series(Y_res), pd.DataFrame(data=X_res)], axis=1)
+result.columns = df_train.columns
 
 # Write to files
 result.to_csv(NAME_OF_SMOTE_OVER_SAMPLING_FILE, encoding='utf-8', index=False)
+print('done')
